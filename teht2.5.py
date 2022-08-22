a9ec7233fc3ed11e38fff6a5a86a318dca2ab9ff
eemiljka <eemiljka@metropolia.fi>
2022-08-22 15:36:20 +0300

    läksy commit

--- teht2.5.py
+++ teht2.5.py
@@ -1,2 +1,17 @@
 # math.floor(grammat/1000)
 
+# luoti 13,3 g
+# naula = 425,6 g
+# leiviskä = 8512 g
+
+leiviska = float(input("Anna leiviskät"))
+naula = float(input("Anna naulat"))
+luoti = float(input("Anna luodit"))
+
+naula2 = (leiviska * 20) + naula
+luoti2 = (naula2 * 32) + luoti
+gramma = (luoti2 * 13.3)
+
+kilogramma = gramma // 1000
+
+
