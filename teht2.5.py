2a87e913ae34041d8f023e245789c0e8ff54c7e4
eemiljka <eemiljka@metropolia.fi>
2022-08-22 17:48:30 +0300

    läksy commit 3

--- teht2.5.py
+++ teht2.5.py
@@ -13,5 +13,7 @@ luoti2 = (naula2 * 32) + luoti
 gramma = (luoti2 * 13.3)
 
 kilogramma = gramma // 1000
+gramma2 = gramma % 1000
 
-
+print(f"Massa nykymittojen mukaan: ")
+print(f"{kilogramma:.0f} kilogrammaa ja {gramma2:.2f} grammaa.")
