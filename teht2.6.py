1e72d065700294cd79a4ceb330eeeb7d41eba1ed
eemiljka <eemiljka@metropolia.fi>
2022-08-22 17:38:08 +0300

    läksy commit 2

--- teht2.6.py
+++ teht2.6.py
@@ -1,5 +1,21 @@
 import random
 
-x = random.randint(0, 9)
-print(x)
+# kolminumeroinen numerosarja
+
+x1 = random.randint(0, 9)
+x2 = random.randint (0, 9)
+x3 = random. randint (0, 9)
+
+print(x1, x2, x3)
+
+
+# nelinumeroinen numerosarja
+
+z1 = random.randint (1, 6)
+z2 = random.randint (1, 6)
+z3 = random.randint (1, 6)
+z4 = random.randint (1, 6)
+
+
+print (z1, z2, z3, z4)
 
