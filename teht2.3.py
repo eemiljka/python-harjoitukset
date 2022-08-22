4f7c84259a14ba597c51f78212ffcedead7ee6c7
eemiljka <eemiljka@metropolia.fi>
2022-08-22 14:45:51 +0300

    kolmasteht commit

---
+++ teht2.3.py
@@ -0,0 +1,12 @@
+# kysytään tiedot
+
+kanta = float(input("Anna suorakulmion kanta"))
+korkeus = float(input("Anna suorakulmion korkeus"))
+
+#laskutoimitukset
+
+A = kanta * korkeus
+piiri = (kanta * 2) + (korkeus * 2)
+
+print(f"Ala = {A}")
+print(f"piiri = {Piiri}")
