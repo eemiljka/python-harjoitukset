ad44f58fa9aa7c8b7c35614794405f82a7c7f47d
eemiljka <eemiljka@metropolia.fi>
2022-08-22 14:52:58 +0300

    neljästeht commit

---
+++ teht2.4.py
@@ -0,0 +1,14 @@
+# syötteet
+luku_1 = int(input("Anna kokonaisluku "))
+luku_2 = int(input("Anna kokonaisluku "))
+luku_3 = int(input("Anna kokonaisluku "))
+
+# laskutoimitukset
+summa = luku_1 + luku_2 + luku_3
+tulo = luku_1 * luku_2 * luku_3
+ka = summa / 3
+
+# tulostus
+print(summa)
+print(tulo)
+print(ka)
