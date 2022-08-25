9f1a2f8e999f3000e96c3aed3c7ca403b8869559
eemiljka <eemiljka@metropolia.fi>
2022-08-25 19:50:47 +0300

    hemoglobiinitehtävän vika commit

--- teht3.3.py
+++ teht3.3.py
@@ -8,3 +8,15 @@ if sukupuoli in ["nainen"]:
     arvo1 = float(input("Kerro hemoglobiiniarvosi (g/l): "))
     if 117 <= arvo1 <= 175:
         print("Hemoglobiiniarvosi on normaali.")
+    if arvo1<117:
+        print("Hemoglobiiniarvosi on alhainen.")
+    if arvo1>175:
+        print("Hemoglobiiniarvosi on korkea.")
+if sukupuoli in ["mies"]:
+    arvo2 = float(input("Kerro Hemoglobiiniarvosi (g/l: "))
+    if 134 <= arvo2 <= 195:
+        print("Hemoglobiiniarvosi on normaali.")
+    if arvo2<134:
+        print("Hemoglobiiniarvosi on alhainen.")
+    if arvo2>195:
+        print("Hemoglobiiniarvosi on korkea.")
