bc3cb37c756e18995f7c888faeeded7b202640dd
eemiljka <eemiljka@metropolia.fi>
2022-08-25 18:58:51 +0300

    hyttitehtävä vika commit

--- teht3.2.py
+++ teht3.2.py
@@ -1,19 +1,15 @@
-# Kirjoita ohjelma, joka kysyy käyttäjältä laivan hyttiluokan (LUX, A, B, C) ja
-# tulostaa sen sanallisen kuvauksen alla olevan luettelon mukaisesti. Tehtävässä
-# on käytettävä if/elif/else-toistorakennetta.
-#
-# LUX on parvekkeellinen hytti yläkannella.
-# A on ikkunallinen hytti autokannen yläpuolella.
-# B on ikkunaton hytti autokannen yläpuolella.
-# C on ikkunaton hytti autokannen alapuolella.
-# Jos käyttäjä syöttää kelvottoman hyttiluokan, ohjelma tulostaa Virheellinen hyttiluokka.
+
 hyttiluokka = input("Mikä on hyttiluokkasi? ")
-if hyttiluokka in ["LUX"] or ["lux"]:
+
+if hyttiluokka in ["LUX"]:
     print("LUX on parvekkeellinen hytti yläkannella.")
-if hyttiluokka in ["A"] or ["a"]:
+if hyttiluokka in ["A"]:
     print("A on ikkunallinen hytti autokannen yläpuolella.")
-if hyttiluokka in ["B"] or ["b"]:
+if hyttiluokka in ["B"]:
     print("B on ikkunaton hytti autokannen yläpuolella.")
-if hyttiluokka in ["C"] or ["c"]:
+if hyttiluokka in ["C"]:
     print("C on ikkunaton hytti autokannen alapuolella.")
 
+# NÄITÄ VIKOJA RIVEJÄ EN SAANUT TOIMIMAAN...
+# else:
+#     print("Virheellinen hyttiluokka.")
