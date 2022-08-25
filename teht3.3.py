5f8dc748f34331eaf85638e776587049cf07a35a
eemiljka <eemiljka@metropolia.fi>
2022-08-25 19:19:54 +0300

    hemoglobiini commit

---
+++ teht3.3.py
@@ -0,0 +1,12 @@
+# Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja
+# hemoglobiiniarvon (g/l). Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen,
+# normaali vai korkea.
+# Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
+# Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+sukupuoli = input("Oletko mies vai nainen: ")
+if sukupuoli in ["nainen"]:
+    arvo1 = float(input("Kerro hemoglobiiniarvosi (g/l): "))
+    if arvo1<117:
+        print("Hemoglobiiniarvosi on alhainen.")
+        if arvo1>175:
+            print("Hemoglobiiniarvosi on korkea.")
