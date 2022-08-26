4a41088acbf28140cedc92026d93cf5a67bc8072
eemiljka <eemiljka@metropolia.fi>
2022-08-26 10:08:07 +0300

    3_TEHT_commit

--- teht3.2.py
+++ teht3.2.py
@@ -1,15 +1,11 @@
-
 hyttiluokka = input("Mikä on hyttiluokkasi? ")
-
 if hyttiluokka in ["LUX"]:
     print("LUX on parvekkeellinen hytti yläkannella.")
-if hyttiluokka in ["A"]:
+elif hyttiluokka in ["A"]:
     print("A on ikkunallinen hytti autokannen yläpuolella.")
-if hyttiluokka in ["B"]:
+elif hyttiluokka in ["B"]:
     print("B on ikkunaton hytti autokannen yläpuolella.")
-if hyttiluokka in ["C"]:
+elif hyttiluokka in ["C"]:
     print("C on ikkunaton hytti autokannen alapuolella.")
-
-# NÄITÄ VIKOJA RIVEJÄ EN SAANUT TOIMIMAAN...
-# else:
-#     print("Virheellinen hyttiluokka.")
+else:
+    print("Virheellinen hyttiluokka.")
---
+++ teht3.4.py
@@ -0,0 +1,6 @@
+# Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa,
+# onko annettu vuosi karkausvuosi. Vuosi on karkausvuosi,
+# jos se on jaollinen neljällä. Sadalla jaolliset vuodet ovat
+# karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
+import datetime
+vuosi = input(int("Anna vuosiluku: "))
