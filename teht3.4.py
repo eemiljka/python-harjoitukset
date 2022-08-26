56c8da7dea9c741dd76e330e0ec08ee360332f55
eemiljka <eemiljka@metropolia.fi>
2022-08-26 23:06:13 +0300

    karkausvuosi commit

--- teht3.4.py
+++ teht3.4.py
@@ -1,6 +1,7 @@
-# Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa,
-# onko annettu vuosi karkausvuosi. Vuosi on karkausvuosi,
-# jos se on jaollinen neljällä. Sadalla jaolliset vuodet ovat
-# karkausvuosia vain jos ne ovat jaollisia myös neljälläsadalla.
-import datetime
-vuosi = input(int("Anna vuosiluku: "))
+# ohjelmalle syötetään vuosiluku, ja se ilmoittaa onko kyseinen vuosi karkausvuosi vai ei
+
+vuosi = float(input("Anna vuosiluku: "))
+if vuosi % 400 == 0 or vuosi % 100 != 0 and vuosi % 4 == 0:
+    print("Vuosi on karkausvuosi.")
+else:
+    print("Vuosi ei ole karkausvuosi.")
