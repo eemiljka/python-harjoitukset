d61a6c1b26b570e0d4becdcbbcae804b741f3797
eemiljka <eemiljka@metropolia.fi>
2022-08-30 10:26:37 +0300

    4.1COMMIT

--- teht4.1.py
+++ teht4.1.py
@@ -2,4 +2,8 @@
 # käyttävä ohjelma,
 # joka tulostaa kolmella jaolliset
 # luvut väliltä 1..1000.
-x = int(1)
+pienin_luku = 1
+suurin_luku = 1000
+for luvut in range(pienin_luku, suurin_luku + 1):
+    if luvut%3==0:
+        print(luvut)
