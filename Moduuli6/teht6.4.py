886a27e7d85f7e0f5fbfae973e6f8c735ef6445c
eemiljka <eemiljka@metropolia.fi>
2022-09-07 22:48:12 +0300

    listateht

--- Moduuli6/teht6.4.py
+++ Moduuli6/teht6.4.py
@@ -3,3 +3,11 @@
 # lukujen summan. Kirjoita testausta varten pääohjelma,
 # jossa luot listan, kutsut funktiota ja tulostat sen
 # palauttaman summan.
+import random
+def numList(luvut):
+    summa = sum(luvut)
+    return summa
+
+if True:
+    lista = random.sample(range(1,100), 10)
+    print(f"Listan summa: {numList(lista)}")
