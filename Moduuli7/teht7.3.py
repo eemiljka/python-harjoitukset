38e7fb8c46320a1005f72b01765f2e79406e7ced
eemiljka <eemiljka@metropolia.fi>
2022-09-11 12:12:34 +0300

    Lentoasema

---
+++ Moduuli7/teht7.3.py
@@ -0,0 +1,20 @@
+user = input("Syötä uusi lentoasema ('1')\nhae aiempaa lentoasemaa('2')\ntai lopeta ('3') ")
+icaoNimi = {"EFHK":"Helsinki-Vantaa",
+            "KJFK":"John F. Kennedy",
+            "00NJ":"Colgate-Piscataway"}
+
+while user != 3:
+
+    if user == "1":
+        uusiKoodi = input("Syötä uusi ICAO-koodi: ").upper()
+        lentokenttä = input("Syötä lentoaseman nimi: ")
+        icaoNimi[uusiKoodi] = lentokenttä
+
+    elif user == "2":
+        ICAO = input("Syötä ICAO-koodi: ").upper()
+        if ICAO in icaoNimi:
+            print(f"ICAO-koodi {ICAO} vastaa lentokenttää: {icaoNimi[ICAO]}")
+
+    user = input("\nSyötä uusi lentoasema ('1')\nhae aiempaa lentoasemaa('2')\ntai lopeta ('3') ")
+
+# Ohjelma ei sulkeudu painamalla '3'...
