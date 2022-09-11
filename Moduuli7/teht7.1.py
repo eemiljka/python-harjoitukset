24da99465fe5b6b93613289b729b5786faea1cfe
eemiljka <eemiljka@metropolia.fi>
2022-09-11 12:28:08 +0300

    vuodenajat

--- Moduuli7/teht7.1.py
+++ Moduuli7/teht7.1.py
@@ -5,19 +5,15 @@ while True:
 
     if user == 3 or user == 4 or user == 5:
         print(vuodenajat[0])
-        break
 
-    if user == 6 or user == 7 or user == 8:
+    elif user == 6 or user == 7 or user == 8:
         print(vuodenajat[1])
-        break
 
-    if user == 9 or user == 10 or user == 11:
+    elif user == 9 or user == 10 or user == 11:
         print(vuodenajat[2])
-        break
 
-    if user == 12 or user == 1 or user == 2:
+    elif user == 12 or user == 1 or user == 2:
         print(vuodenajat[3])
-        break
 
-    else:
+    elif user < 1 or user > 12:
         print("Virheellinen arvo...")
