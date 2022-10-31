f31c036939adf0a5d29684a3d2f50f39dfe99f06
eemiljka <eemiljka@metropolia.fi>
2022-10-31 23:37:55 +0200

    10.3tehty

--- Moduuli10/teht10.3.py
+++ Moduuli10/teht10.3.py
@@ -1,56 +1,81 @@
-# KOODI EI TOIMI
-
-# Luodaan hissi-luokka, jolla 3 metodia: Siirry kerrokseen, kerros ylös ja kerros alas.
-class Hissi:
-    def __init__(self, alinKerros, ylinKerros):
+class Talo:
+    def __init__(self, alinKerros, ylinKerros, hissiLKM):
+        self.hissit = []
         self.alinKerros = alinKerros
         self.ylinKerros = ylinKerros
-        self.kerros = self.alinKerros
+        self.hissiLKM = hissiLKM
 
-    def siirryKerrokseen(self, uusiKerros):
-        if uusiKerros > self.kerros:
-            while self.kerros < uusiKerros:
-                hissi1.kerrosYlös()
-        elif uusiKerros < self.kerros:
-            while self.kerros > uusiKerros:
-                hissi1.kerrosAlas()
+        for i in range(hissiLKM):
+            self.hissit.append(Hissi(alinKerros, ylinKerros, i + 1))
+
+    def print_data(self):
+        print(f"\nTalon ylin kerros: {self.ylinKerros}")
+        print(f"Talon alin kerros: {self.alinKerros}")
+        print(f"Hissien lukumäärä: {self.hissiLKM}")
+
+        for i in self.hissit:
+            i.print_info()
+
+    def aja_hissia(self, hissiNumero, kohdekerros):
+        elevator = self.hissit[hissiNumero - 1]
+        print(f"Ajetaan hissiä {hissiNumero}\n")
+        elevator.go_to_floor(kohdekerros)
+
+class Hissi:
+    def __init__(self, alin, ylin, numero):
+        self.nbr = numero
+        self.alin = alin
+        self.ylin = ylin
+        self.nykyinen = alin
+
+    def print_info(self):
+        print(f"\nHissi {self.nbr}: ")
+        print(f"Hissin ylin kerros: {self.ylin}")
+        print(f"Hissin alin kerros: {self.alin}")
+        print(f"Hissin nykyinen kerros: {self.nykyinen}\n")
+
+    def go_to_floor(self, kerros):
+
+        if self.alin > kerros > self.ylin:
+            self.nykyinen = self.nykyinen
+
+        elif self.alin <= kerros <= self.ylin:
+
+            if self.nykyinen < kerros:
+                x = self.alin
+
+                while x <= kerros:
+                    Hissi.floor_up(self, kerros)
+                    x = x + 1
+
+            elif self.nykyinen > kerros:
+                x = self.ylin
+
+                while x >= kerros:
+                    Hissi.floor_down(self, kerros)
+                    x = x - 1
+
+    def floor_up(self, kerros):
+        if self.nykyinen < kerros:
+            self.nykyinen = self.nykyinen + 1
+            print(f"Hissi nro{self.nbr} on kerroksessa: {self.nykyinen}")
+
+    def floor_down(self, kerros):
+        if self.nykyinen > kerros:
+            self.nykyinen = self.nykyinen - 1
+            print(f"Hissi nro{self.nbr} on kerroksessa: {self.nykyinen}")
 
 
-    def kerrosYlös(self):
-        self.kerros = self.kerros + 1
+if __name__ == '__main__':
+    lowest_floor = 1
+    top_floor = 50
+    hissi_lkm = 5
 
-    def kerrosAlas(self):
-        self.kerros = self.kerros - 1
+    house = Talo(lowest_floor, top_floor, hissi_lkm)
+    house.print_data()
 
-class Talo:
-    def __init__(self, talonAlin, talonYlin, hissienLKM):
-        self.talonAlin = talonAlin
-        self.talonYlin = talonYlin
-        self.hissienLKM = hissienLKM
-        self.hissit = []
-        self.hissit.append(hissienLKM)
+    elevator_to_drive = 2
+    floor_to_go = 15
 
-    def aja_hissiä(self, hissi_numero, kohdekerros):
-        self.hissi_numero = hissi_numero
-        self.kohdekerros = kohdekerros
-        for hissi in self.hissit:
-            hissi.siirryKerrokseen(kohdekerros)
-
-    def palohälytys(self):
-        self.hissit.siirryKerrokseen(0)
-
-
-# Pääohjelma
-hissi1 = Hissi(0, 7)
-print("Nyt olemme kerroksessa: ", hissi1.kerros)
-
-# Liikutaan hissillä ensin viidenteen kerrokseen. Sen jälkeen alimpaan kerrokseen.
-hissi1.siirryKerrokseen(5)
-print("Hissi siirtyi kerrokseen: ", hissi1.kerros)
-hissi1.siirryKerrokseen(hissi1.alinKerros)
-print("Hissi siirtyi alimpaan kerrokseen: ", hissi1.kerros)
-
-# Luodaan talo ja lause talon hisseillä ajelemiseksi
-talo1 = Talo(0, 7, 1)
-talo1.aja_hissiä(0, 7)
-
+    house.aja_hissia(elevator_to_drive, floor_to_go)
+    house.print_data()
