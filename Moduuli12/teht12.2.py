bd2971fdec2d7235cc5a26c78e1af1c2d97c73df
eemiljka <eemiljka@metropolia.fi>
2022-11-07 19:04:51 +0200

    Mod12 teht2 DONE

--- Moduuli12/teht12.2.py
+++ Moduuli12/teht12.2.py
@@ -1,10 +1,15 @@
+# This program asks the user a city then prints that city's temperature using API.
 import requests
 
 API_key = "9f27369183880d8c5a9e9d0f84c41d98"
 
 city = input("Enter a city or press 'Enter' to quit program: ")
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_key+"&q"+city
-weather_data = requests.get(base_url).json()
-print(weather_data)
+request = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_key
 
-#TODO: venaa, että API key toimii ja sitten koodaa loppuun.
+response = requests.get(request)
+json_response = response.json()
+x = response.json()
+y = x["main"]
+temperature = y["temp"]
+
+print(city, "temperature is", temperature - 273.15, "celsius at this moment")
